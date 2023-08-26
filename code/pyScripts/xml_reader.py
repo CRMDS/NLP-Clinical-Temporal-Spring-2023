f2ff3ca9abede01a18dfce6dda7cf621e2b12485
Laura <laura.pierce@outlook.com>
2023-08-26 17:36:42 +1000

    Fixed issue in open() function

--- code/pyScripts/xml_reader.py
+++ code/pyScripts/xml_reader.py
@@ -8,13 +8,13 @@ import glob
 # Collect all paths of all .xml files
 paths = glob.glob("/home/laura/Documents/Spring2023/NLP_Data/Tempor/trainingJuly2012/*.xml")
 
-# Loop this to make it automated
+# Lists to hold the text/tags. Not sure if this is the best object
 textClin = []
 tagsClin = []
 
 def main():
     for i in range(0, (len(paths))):
-        with open(paths[93], 'r') as f:
+        with open(paths[i], 'r') as f:
             data = f.read()
             # Extract out the original clinical notes
             bs_data = BeautifulSoup(data, "xml")
@@ -25,7 +25,8 @@ def main():
             clin_tags = bs_data.find('TAGS')
             tagsClin.append(clin_tags)
 
-    print(textClin[2])
+    # print out random text to check
+    print(textClin[8])
 
 if __name__ == '__main__':
     main()
